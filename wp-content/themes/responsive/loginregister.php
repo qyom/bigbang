d120b9214ae936d4984e9dabaa8dea602d80ba28
haykz7777 <hz@HZAKAR>
2012-11-09 23:07:27 -0800

    new change for login/register custom page template

---
+++ wp-content/themes/responsive/loginregister.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+Template Name: LoginRegister
+*/
+
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+
+?>
+<?php get_header(); ?>
+
+        <div id="content" class="grid col-620">
+        
+<?php if (have_posts()) : ?>
+
+		<?php while (have_posts()) : the_post(); ?>
+		
+     
+		
+        <?php $options = get_option('responsive_theme_options'); ?>
+		<?php if ($options['breadcrumb'] == 0): ?>
+		<?php echo responsive_breadcrumb_lists(); ?>
+        <?php endif; ?>
+        
+            <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <h1><?php the_title(); ?></h1>
+				
+				<!-- below added by Hayk Zakaryan  -->
+				<?php  echo login_with_ajax();   ?>
+ 
+                <?php if ( comments_open() ) : ?>               
+				
+				
+                <!-- end of .post-meta -->
+                <?php endif; ?> 
+                
+                <div class="post-entry">
+                    <?php the_content(__('Read more &#8250;', 'responsive')); ?>
+                    <?php wp_link_pages(array('before' => '<div class="pagination">' . __('Pages:', 'responsive'), 'after' => '</div>')); ?>
+                </div><!-- end of .post-entry -->
+                
+                <?php if ( comments_open() ) : ?>
+                <div class="post-data">
+				    <?php the_tags(__('Tagged with:', 'responsive') . ' ', ', ', '<br />'); ?> 
+                    <?php the_category(__('Posted in %s', 'responsive') . ', '); ?> 
+                </div><!-- end of .post-data -->
+                <?php endif; ?>             
+            
+            <div class="post-edit"><?php edit_post_link(__('Edit', 'responsive')); ?></div> 
+            </div><!-- end of #post-<?php the_ID(); ?> -->
+            
+            <?php //comments_template( '', true ); ?>
+            
+        <?php endwhile; ?> 
+        
+        <?php if (  $wp_query->max_num_pages > 1 ) : ?>
+        <div class="navigation">
+			<div class="previous"><?php next_posts_link( __( '&#8249; Older posts', 'responsive' ) ); ?></div>
+            <div class="next"><?php previous_posts_link( __( 'Newer posts &#8250;', 'responsive' ) ); ?></div>
+		</div><!-- end of .navigation -->
+        <?php endif; ?>
+
+	    <?php else : ?>
+
+        <h1 class="title-404"><?php _e('404 &#8212; Fancy meeting you here!', 'responsive'); ?></h1>
+        <p><?php _e('Don&#39;t panic, we&#39;ll get through this together. Let&#39;s explore our options here.', 'responsive'); ?></p>
+        <h6><?php _e( 'You can return', 'responsive' ); ?> <a href="<?php echo home_url(); ?>/" title="<?php esc_attr_e( 'Home', 'responsive' ); ?>"><?php _e( '&larr; Home', 'responsive' ); ?></a> <?php _e( 'or search for the page you were looking for', 'responsive' ); ?></h6>
+        <?php get_search_form(); ?>
+
+<?php endif; ?>  
+      
+        </div><!-- end of #content -->
+
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
